7ecc7dcc3c0f9bda43858fd219eb0090de12975e
=fandeer <fandeerhu@gmail.com>
2024-01-24 13:45:05 +0800

    123

--- openAPI/main1.py
+++ openAPI/main1.py
@@ -13,19 +13,24 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id:int}")
-async def get_item(item_id):
+@app.get("/items/{item_id}")
+async def get_item(item_id:int):
     print(f"使用者輸入了:{item_id}")
     return {"item_id":item_id}
+
 @app.get("/items/{date}/{celsius}")
 async def get_item(date:str,celsius:float):
     print(f"日期:{date}")
     print(f"溫度:{celsius}")
     return {"日期":date,"攝氏溫度":celsius}
+
+
+
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
 
 @app.get("/pico_w/{date}")
 async def read_item(date:str ,address:str,celsius:float=0.0):
@@ -33,3 +38,4 @@ async def read_item(date:str ,address:str,celsius:float=0.0):
     print(f"位置:{address}")
     print(f"攝氏:{celsius}")
     return {"狀態":"儲存成功"}
+
