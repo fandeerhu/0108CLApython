6e5eda58cde40ee9f0c80179d1ed0ed39ed07046
=fandeer <fandeerhu@gmail.com>
2024-01-26 11:56:24 +0800

    modify

---
+++ UI/main.py
@@ -0,0 +1,27 @@
+import requests
+import pandas as pd
+import streamlit as st
+from streamlit_autorefresh import st_autorefresh
+
+
+st_autorefresh(interval=10000, limit=100, key="fizzbuzzcounter")
+url = 'https://openapi-test-6vnu.onrender.com/pico_w/?count=5'
+
+r = requests.get(url=url)
+
+if r.status_code == 200:
+    print("下載成功")
+    data = r.json()
+
+dataFrame = pd.DataFrame(data)
+
+st.header("學院養雞場")
+st.divider()
+st.caption("溫度-光線表表😍")
+st.write(dataFrame)
+st.divider()
+st.caption("光線")
+st.line_chart(dataFrame,x='date',y='light')
+st.divider()
+st.caption("溫度")
+st.line_chart(dataFrame,x='date',y='temperature',color='#ff0000')
